1c0d19d53a2e230452984e036f6880952e931ce7
Matheus Vinicius Silva Miranda Pimentel <52512357+MatheusVSMPimentel@users.noreply.github.com>
2021-06-07 22:31:43 -0300

    Cliente model project class

---
+++ ContaBanco/Cliente.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace POOStart
+{
+public class Cliente 
+  {
+    public string nome;
+    public string cpf;
+    public string rg;
+    public string endereco;
+  }
+}
