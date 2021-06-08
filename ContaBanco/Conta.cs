68cbded48fb5b6a0beff036b28467211335851d7
Matheus Vinicius Silva Miranda Pimentel <52512357+MatheusVSMPimentel@users.noreply.github.com>
2021-06-07 22:33:11 -0300

    Conta model class

---
+++ ContaBanco/Conta.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using POOStart.Cliente;
+using POOStart.Conta;
+
+namespace POOStart
+{
+
+public class Conta
+	{
+		public int numero;
+    public Cliente titular;
+    public double saldo;
+    public int idade;
+    
+    public bool Saca(double valorASacar)
+    {
+      if(this.saldo >= valorASacar && valorASacar > 0 && this.idade > 13)
+      {
+        if(this.idade > 18)
+        {
+        this.saldo -= valorASacar;
+        return true;
+        } else if (valorASacar < 200)
+        {
+        this.saldo -= valorASacar;
+        return true;
+        } else 
+        {
+          return false;
+        }
+        
+      }
+      else
+      {
+        return false;
+      }
+    }
+    
+    public void Deposita(double valor)
+    {
+      this.saldo += valor;
+    }
+    
+    public void Transfere(double valor, Conta destino)
+    {
+      if(this.Saca(valor))
+      {
+        destino.Deposita(valor);
+      }
+    }
+    
+	}
+}
